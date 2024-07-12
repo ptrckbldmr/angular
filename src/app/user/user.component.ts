b8f5ada70b6d2600cdce7e1b59e6a9212e43d371
unknown <patrickbaldomar@gmail.com>
2024-07-12 15:05:00 +0800

    second

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+// import { Component,computed, signal } from '@angular/core';
+// import { DUMMY_USERS } from '../dummy-users';
+
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
 
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar
+  // }
+
+  @Input() avatar!: string;
+  @Input() name!: string;
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar
+  }
+
+  onSelectUser() {
+    // console.log("Clicked!")
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
 }
